887fb5b0a3615a8d701fc65ff03e12e1122da7bd
Szymon <szymszello@gmail.com>
2017-07-25 13:19:47 +0200

    dupa

---
+++ faktury/src/Controller/faktury.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\faktury\Controller;
+
+use Drupal\Core\ebiSoapEBOK;
+use Drupal\Core\Controller\ControllerBase;
+
+class faktury extends ControllerBase
+{
+    public function wyswietl()
+    {
+        $soap = new ebiSoapEBOK();
+        $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+        $name = $user->get('name')->value;
+        $output = array();
+        $pola = array('name','kod_miesiaca','odczyt');
+
+        $wynik = $soap->pobierz_rekordy('ebok_faktury', $pola, 'ebok_faktury.nazwa_klienta ='."'$name'");
+        // $wynik = $soap->pobierz_rekordy('ebok_liczniki');
+        $wynik = json_encode($wynik);
+
+        array_push($output,'<div id=asd>'.$wynik.'</div>');
+        array_push($output, '<table id = "example" class="display" cellspacing="0" width="100%"></table>');
+
+        return array('#markup'=>implode($output,''));
+
+    }
+
+}
---
+++ zgloszenia_form/src/Form/ZgloszeniaForm.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\zgloszenia_form\Form;
+
+use Drupal\Core\ebiSoapEBOK;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class ZgloszeniaForm extends FormBase
+{
+
+    /**
+     * metody dziedziczone z klasy FormBase
+     * @return string
+     */
+    public function getFormId()
+    {
+        return 'zgloszenia_form';
+    }
+
+
+    /**
+     * Buduje formularz https://www.drupal.org/docs/8/api/form-api
+     * @param array $form
+     * @param FormStateInterface $form_state
+     * @return array
+     */
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        $form['typ_zgloszenia'] = [
+            '#type' => 'select',
+            '#title' => t('Typ zgłoszenia'),
+            '#required' => 'TRUE',
+            '#options' => [
+                '#zepsuty_licznik' => t('Zepsuty licznik'),
+                '#wyciek' => t('Wyciek rury'),
+                '#inne' => t('Inne')
+            ]
+        ];
+
+        $form['wiadomosc'] = [
+            '#type' => 'textarea',
+            '#required' => 'TRUE',
+            '#rows' => 20,
+            '#title' => t('Wiadomość')
+        ];
+
+        $form['actions']['#type'] = 'actions';
+        $form['actions']['submit'] = array(
+            '#type' => 'submit',
+            '#value' => $this->t('Wyślij'),
+            '#button_type' => 'primary',
+        );
+
+        return $form;
+    }
+
+    /**
+     * tutaj dodajesz obsluge danych
+     * @param array $form
+     * @param FormStateInterface $form_state tutaj jest formularz jako tablica asocjacyjna
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id()); //load current user
+        $name = $user->get('name')->value; //load current user login
+        $typ_zgloszenia = $form_state->getValue('typ_zgloszenia');
+        $tresc = $form_state->getValue('wiadomosc');
+        $soap_request = new ebiSoapEBOK();
+        $tab_danych = array(
+            'typ'=>$typ_zgloszenia,
+            'opis'=>$tresc,
+            'autor'=>$name,
+            'stan'=>'w toku'
+        );
+        $soap_request->wyslij_rekord('ebok_zgloszenia',$tab_danych);
+    }
+}
